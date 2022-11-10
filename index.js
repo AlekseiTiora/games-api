31cb37bc5db637210048f7b3808c82d27cd2dbc1
opilane <pump@gmail.com>
2022-11-10 13:20:05 +0200

    As an app developer I want to get a list of all games to show in my app[Delivers #183767708]

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const app = require('express')()
 const port = 8080
 const swaggerUi = require ('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json');
+const yamljs=require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.yaml')
+
+const games=[
+    {id:1,name:"Witcher 3", price: 29.99},
+    {id:2,name:"Cyberpunk 2077", price: 59.99},
+    {id:3,name:"Minecraft", price: 5.00},
+    {id:4,name:"cs:go", price: 18.70},
+    {id:5,name:"Roblox", price: 1.00},
+    {id:6,name:"Valorant", price: 15.00},
+    {id:7,name:"GTA5", price: 30.00},
+]
+
+
 
 app.get('/games',(req,res) => {
-    res.send(["nhl22", "minecraft"])
+    res.send(games)
+})
+app.get('/games/:id', (req,res)=>{
+    if (typeof games[req.params.id - 1] === 'undefined'){
+        return res.status(404).send({error:"Game not found"})
+    }
+    res.send(games[req.params.id - 1])
+})
+
+app.post('/games',(req,res) =>{
+    games.push({
+        id:games.length+1,
+        price: req.body.price,
+        name:req.body.name
+    })
+    res.end()
 })
 
 app.use('/docs', swaggerUi.serve,swaggerUi.setup(swaggerDocument))
